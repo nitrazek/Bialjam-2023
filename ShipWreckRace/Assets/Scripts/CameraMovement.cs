ee8e4ce28cf084b016d0302cb79a3a6965508564
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 15:09:24 +0200

    Added scene

--- ShipWreckRace/Assets/Scripts/CameraMovement.cs
+++ ShipWreckRace/Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraMovement : MonoBehaviour
 {
-    private 
+    public Transform targetTransform;
+    private Vector3 offset;
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        offset = transform.position - targetTransform.position;
     }
 
     void FixedUpdate()
