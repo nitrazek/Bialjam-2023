28684c557abfaaa9554b990d27d6d7b487931b8c
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 18:47:11 +0200

    Movement changes

--- ShipWreckRace/Assets/Scripts/PlayerMovement.cs
+++ ShipWreckRace/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int tiltForce = 20;
-    public int pushForce = 5;
+    public float forceMultiplier = 10f;
     private Rigidbody rb;
 
     void Start()
@@ -14,24 +13,36 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        bool keyA = Input.GetKey(KeyCode.A);
-        bool keyD = Input.GetKey(KeyCode.D);
+        // Sprawdzanie naciœniêcia klawiszy 'a' i 'd'
+        bool moveLeft = Input.GetKey(KeyCode.A);
+        bool moveRight = Input.GetKey(KeyCode.D);
 
-        if(keyA && !keyD)
+        // Je¿eli naciœniêto oba klawisze jednoczeœnie
+        if (moveLeft && moveRight)
         {
-            rb.AddForce(Vector3.left * tiltForce);
-            rb.AddForce(Vector3.forward * pushForce);
+            //skakanie
         }
-        else if(!keyA && keyD)
+        else
         {
-            rb.AddForce(Vector3.right * tiltForce);
-            rb.AddForce(Vector3.forward * pushForce);
+            // Poruszanie siê w lewo
+            if (moveLeft)
+            {
+                ApplyForce(Vector3.left);
+            }
+
+            // Poruszanie siê w prawo
+            if (moveRight)
+            {
+                ApplyForce(Vector3.right);
+            }
         }
-        else if(keyA && keyD)
-        {
-            rb.AddForce(Vector3.up * tiltForce * 10);
-        }
+    }
+    void ApplyForce(Vector3 direction)
+    {
+        // Obliczanie si³y na podstawie kierunku i mno¿nika si³y
+        Vector3 force = direction * forceMultiplier * Time.deltaTime;
+        rb.AddForce(force);
     }
 }
