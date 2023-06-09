dd78146938c818552fcb8b1c361939ec78691cc0
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 23:38:09 +0200

    Added drakkar movement

--- ShipWreckRace/Assets/Scripts/PlayerMovement.cs
+++ ShipWreckRace/Assets/Scripts/PlayerMovement.cs
@@ -5,5 +5,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    private float speed = 50f;
+    private float maxAngle;
+    private float minAngle;
+    private int rotateCounter;
+
+    void Start()
+    {
+        float y = transform.rotation.y;
+        minAngle = y + 45f;
+        maxAngle = y - 45f;
+        rotateCounter = 0;
+    }
+
+    void Update()
+    {
+        float axis = Input.GetAxis("Horizontal");
+        if (axis != 0)
+        {
+            float rotation = axis * speed * Time.deltaTime;
+            Debug.Log("Rotacja: " + rotation);
+            rotateCounter = rotateCounter + 1;
+            Debug.Log("Counter: " + rotateCounter);
+            transform.Rotate(0f, 0f, rotation);
+
+            /*Quaternion currentRotation = transform.rotation;
+            Vector3 currentEulerAngles = currentRotation.eulerAngles;
+            float clampedAngle = Mathf.Clamp(currentEulerAngles.z, minAngle, maxAngle);
+            currentEulerAngles.z = clampedAngle;
+            transform.rotation = Quaternion.Euler(currentEulerAngles);*/
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Dupa");
+    }
 }
