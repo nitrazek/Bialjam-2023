20aeb69dabebdf54500a6ced3187a636109d0b22
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 15:04:40 +0200

    Added bean

---
+++ ShipWreckRace/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
