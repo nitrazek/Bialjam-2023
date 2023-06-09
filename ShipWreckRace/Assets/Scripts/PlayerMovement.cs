d76566146515a3d1593752f34e8e5b07738120fd
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 19:37:59 +0200

    Update PlayerMovement.cs

--- ShipWreckRace/Assets/Scripts/PlayerMovement.cs
+++ ShipWreckRace/Assets/Scripts/PlayerMovement.cs
@@ -5,33 +5,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int tiltForce = 20;
-    public int pushForce = 5;
-    private Rigidbody rb;
+    public float forwardSpeed = 5f; // Prêdkoœæ poruszania siê do przodu
+    public float rotationSpeed = 3f; // Prêdkoœæ skrêcania
+    public float maxTiltAngle = 20f; // Maksymalny k¹t przechylenia ³ódki
+    public float tiltSpeed = 2f; // Prêdkoœæ przechylania ³ódki
 
-    void Start()
+    private Rigidbody boatRigidbody;
+    private float inputHorizontal;
+    private Vector3 previousVelocity;
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        boatRigidbody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        bool keyA = Input.GetKey(KeyCode.A);
-        bool keyD = Input.GetKey(KeyCode.D);
+        // Pobieranie wejœcia od gracza
+        inputHorizontal = Input.GetAxis("Horizontal");
+    }
 
-        if(keyA && !keyD)
+    private void FixedUpdate()
+    {
+        // Pobieranie prêdkoœci ³ódki przed zastosowaniem si³
+        previousVelocity = boatRigidbody.velocity;
+
+        // Poruszanie ³ódk¹ do przodu
+        boatRigidbody.AddForce(transform.forward * forwardSpeed);
+
+        // Odpychanie siê wios³ami
+        if (Mathf.Abs(inputHorizontal) > 0)
         {
-            rb.AddForce(Vector3.left * tiltForce);
-            rb.AddForce(Vector3.forward * pushForce);
+            // Dodawanie si³y w bok (zale¿nie od kierunku wios³a)
+            Vector3 sideForce = transform.right * inputHorizontal * forwardSpeed;
+            boatRigidbody.AddForce(sideForce);
+
+            // Skrêcanie ³ódki
+            Quaternion targetRotation = Quaternion.LookRotation(previousVelocity + sideForce, transform.up);
+            boatRigidbody.rotation = Quaternion.Lerp(boatRigidbody.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+
+            // Przechylanie ³ódki na boki
+            float targetTiltAngle = -inputHorizontal * maxTiltAngle;
+            Quaternion targetTilt = Quaternion.Euler(0f, 0f, targetTiltAngle);
+            boatRigidbody.rotation = Quaternion.Lerp(boatRigidbody.rotation, targetTilt, tiltSpeed * Time.fixedDeltaTime);
         }
-        else if(!keyA && keyD)
+        else
         {
-            rb.AddForce(Vector3.right * tiltForce);
-            rb.AddForce(Vector3.forward * pushForce);
-        }
-        else if(keyA && keyD)
-        {
-            rb.AddForce(Vector3.up * tiltForce * 10);
+            // Resetowanie przechylenia ³ódki
+            Quaternion targetTilt = Quaternion.Euler(0f, 0f, 0f);
+            boatRigidbody.rotation = Quaternion.Lerp(boatRigidbody.rotation, targetTilt, tiltSpeed * Time.fixedDeltaTime);
         }
     }
 }
