060c3b7a8ec9a0bce5f7bc8065f60b154025ff9e
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-09 17:32:46 +0200

    First version of movment

--- ShipWreckRace/Assets/Scripts/PlayerMovement.cs
+++ ShipWreckRace/Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int tiltForce = 20;
+    public int pushForce = 5;
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
+        bool keyA = Input.GetKey(KeyCode.A);
+        bool keyD = Input.GetKey(KeyCode.D);
 
+        if(keyA && !keyD)
+        {
+            rb.AddForce(Vector3.left * tiltForce);
+            rb.AddForce(Vector3.forward * pushForce);
         }
-
-        if(Input.GetKey(KeyCode.S))
+        else if(!keyA && keyD)
         {
-
+            rb.AddForce(Vector3.right * tiltForce);
+            rb.AddForce(Vector3.forward * pushForce);
         }
-
-        if(Input.GetKey(KeyCode.A))
+        else if(keyA && keyD)
         {
-            
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-
+            rb.AddForce(Vector3.up * tiltForce * 10);
         }
     }
 }
