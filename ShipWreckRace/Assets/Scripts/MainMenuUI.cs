08c318936875508f9595dcfce3f738ca1a8c6307
xShyQx <bartosz.kar@wp.pl>
2023-06-09 20:03:23 +0200

    Added MainMenuScene

---
+++ ShipWreckRace/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        settingsButton.onClick.AddListener(() =>
+        {
+            // przechodzi na scenê z ustawieniami
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+}
