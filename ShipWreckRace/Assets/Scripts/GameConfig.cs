39cac4e180c96e05d817f18b52eaa217d9f9a5bc
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-10 11:56:52 +0200

    Added more keys

--- ShipWreckRace/Assets/Scripts/GameConfig.cs
+++ ShipWreckRace/Assets/Scripts/GameConfig.cs
@@ -6,6 +6,12 @@ public class GameConfig : MonoBehaviour
 {
     public static KeyCode p1_left = KeyCode.A;
     public static KeyCode p1_right = KeyCode.D;
+    public static KeyCode p1_forward = KeyCode.W;
+    public static KeyCode p1_back = KeyCode.S;
+
+
     public static KeyCode p2_left = KeyCode.LeftArrow;
     public static KeyCode p2_right = KeyCode.RightArrow;
+    public static KeyCode p2_forward = KeyCode.UpArrow;
+    public static KeyCode p2_back = KeyCode.DownArrow;
 }
--- ShipWreckRace/Assets/Scripts/KeyBindingsUI.cs
+++ ShipWreckRace/Assets/Scripts/KeyBindingsUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +9,34 @@ public class KeyBindingsUI : MonoBehaviour
 {
     [SerializeField] private Button p1_leftInput;
     [SerializeField] private Button p1_rightInput;
+    [SerializeField] private Button p1_forwardInput;
+    [SerializeField] private Button p1_backInput;
     [SerializeField] private Button p2_leftInput;
     [SerializeField] private Button p2_rightInput;
+    [SerializeField] private Button p2_forwardInput;
+    [SerializeField] private Button p2_backInput;
     private int lastButtonPressedIndex = 0;
     void Start()
     {
+        mapKey(0, p1_leftInput, GameConfig.p1_left.ToString());
+        mapKey(0, p1_rightInput, GameConfig.p1_right.ToString());
+        mapKey(0, p1_forwardInput, GameConfig.p1_forward.ToString());
+        mapKey(0, p1_backInput, GameConfig.p1_back.ToString());
+
+        mapKey(0, p2_leftInput, GameConfig.p2_left.ToString());
+        mapKey(0, p2_rightInput, GameConfig.p2_right.ToString());
+        mapKey(0, p2_forwardInput, GameConfig.p2_forward.ToString());
+        mapKey(0, p2_backInput, GameConfig.p2_back.ToString());
+
         p1_leftInput.onClick.AddListener(() => mapKey(1, p1_leftInput));
         p1_rightInput.onClick.AddListener(() => mapKey(2, p1_rightInput));
-        p2_leftInput.onClick.AddListener(() => mapKey(3, p2_leftInput));
-        p2_rightInput.onClick.AddListener(() => mapKey(4, p2_rightInput));
+        p1_forwardInput.onClick.AddListener(() => mapKey(3, p1_forwardInput));
+        p1_backInput.onClick.AddListener(() => mapKey(4, p1_backInput));
+
+        p2_leftInput.onClick.AddListener(() => mapKey(5, p2_leftInput));
+        p2_rightInput.onClick.AddListener(() => mapKey(6, p2_rightInput));
+        p2_forwardInput.onClick.AddListener(() => mapKey(7, p2_forwardInput));
+        p2_backInput.onClick.AddListener(() => mapKey(8, p2_backInput));
     }
 
     void Update()
@@ -63,7 +83,6 @@ public class KeyBindingsUI : MonoBehaviour
         lastButtonPressedIndex = buttonIndex;
         TextMeshProUGUI mesh = buttonObj.GetComponentInChildren<TextMeshProUGUI>();
         if (!mesh) return;
-        Debug.Log(text);
-        mesh.text = text.ToUpperInvariant();
+        mesh.text = text.ToLower().FirstCharacterToUpper();
     }
 }
