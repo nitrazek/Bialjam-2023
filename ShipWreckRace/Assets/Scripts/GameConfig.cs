62f5390e2f15abecb707c07e63ef498300374ef3
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-10 09:57:26 +0200

    Added gameconfig and keybindings

---
+++ ShipWreckRace/Assets/Scripts/GameConfig.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfig : MonoBehaviour
+{
+    public static KeyCode p1_left = KeyCode.A;
+    public static KeyCode p1_right = KeyCode.D;
+    public static KeyCode p2_left = KeyCode.LeftArrow;
+    public static KeyCode p2_right = KeyCode.RightArrow;
+}
---
+++ ShipWreckRace/Assets/Scripts/KeyBindingsUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBindingsUI : MonoBehaviour
+{
+    [SerializeField] private Button p1_leftInput;
+    [SerializeField] private Button p1_rightInput;
+    [SerializeField] private Button p2_leftInput;
+    [SerializeField] private Button p2_rightInput;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
