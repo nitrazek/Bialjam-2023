28a075303c82d8b8aed1d7e0c4a78d6f061d6755
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-10 16:16:30 +0200

    New camera

--- ShipWreckRace/Assets/Scripts/CameraMovement.cs
+++ ShipWreckRace/Assets/Scripts/CameraMovement.cs
@@ -6,16 +6,13 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform camTarget;
     public float pLerp = .02f;
-    public float rLerp = .01f;
     public Vector3 offset;
-    public Vector3 rotationOffset;
 
-    void LateUpdate()
+    private void LateUpdate()
     {
-        Vector3 targetPosition = camTarget.position - camTarget.forward * offset.z + camTarget.up * offset.y + camTarget.right * offset.x;
-        Quaternion targetRotation = camTarget.rotation * Quaternion.Euler(rotationOffset);
-
+        Vector3 targetPosition = camTarget.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetPosition, pLerp);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rLerp);
+
+        transform.LookAt(camTarget);
     }
 }
