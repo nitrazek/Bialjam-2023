8ea17659bc806ba2ab7867067d0fd518cce837bc
xShyQx <bartosz.kar@wp.pl>
2023-06-10 12:24:38 +0200

    Fixed movement again

--- ShipWreckRace/Assets/Scripts/PlayerMovement.cs
+++ ShipWreckRace/Assets/Scripts/PlayerMovement.cs
@@ -15,11 +15,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //if (Input.GetKey(GameConfig.p1_forward))
+        //{
+        //    Debug.Log(Input.GetKey(GameConfig.p1_forward));
+        //    Vector3 movement = moveSpeed * transform.TransformDirection(Vector3.right);
+        //    rb.AddForce(movement, ForceMode.Impulse);
+        //} 
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        Debug.Log("Horizontal: " + horizontalInput);
+        Debug.Log("Vertical: " + verticalInput);
+
         Vector3 movement = verticalInput * moveSpeed * transform.TransformDirection(Vector3.right);
-        rb.AddForce(movement);
+        rb.AddForce(movement, ForceMode.Impulse);
 
         transform.Rotate(horizontalInput * transform.TransformDirection(Vector3.up));
     }
