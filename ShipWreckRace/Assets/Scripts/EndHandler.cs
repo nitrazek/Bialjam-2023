7f5f8f753c6303ec1abd1c33e3680745b838b3f7
xShyQx <bartosz.kar@wp.pl>
2023-06-11 04:51:13 +0200

    Fixed counting

--- ShipWreckRace/Assets/Scripts/EndHandler.cs
+++ ShipWreckRace/Assets/Scripts/EndHandler.cs
@@ -18,13 +18,13 @@ public class EndHandler : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.name == "P1_Cube")
+        if (other.name == "P1_Cube")
         {
             if (winner == 0) winner = 1;
             p1_camera.GetComponent<camera>().updatePosition = false;
 
         }
-        else if(other.name == "P2_Cube")
+        else if (other.name == "P2_Cube")
         {
             if (winner == 0) winner = 2;
             p2_camera.GetComponent<camera>().updatePosition = false;
