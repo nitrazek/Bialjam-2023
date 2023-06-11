5c500899f588f2be5a0bdffea064689e698a8bc0
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-06-11 10:51:50 +0200

    Update KeyBindingsUI.cs

--- ShipWreckRace/Assets/Scripts/KeyBindingsUI.cs
+++ ShipWreckRace/Assets/Scripts/KeyBindingsUI.cs
@@ -12,10 +12,12 @@ public class KeyBindingsUI : MonoBehaviour
     [SerializeField] private Button p1_rightInput;
     [SerializeField] private Button p1_forwardInput;
     [SerializeField] private Button p1_backInput;
+    [SerializeField] private Button p1_resetInput;
     [SerializeField] private Button p2_leftInput;
     [SerializeField] private Button p2_rightInput;
     [SerializeField] private Button p2_forwardInput;
     [SerializeField] private Button p2_backInput;
+    [SerializeField] private Button p2_resetInput;
     [SerializeField] private Button returnBtn;
     private int lastButtonPressedIndex = 0;
     void Start()
@@ -24,21 +26,25 @@ public class KeyBindingsUI : MonoBehaviour
         mapKey(0, p1_rightInput, GameConfig.p1_right.ToString());
         mapKey(0, p1_forwardInput, GameConfig.p1_forward.ToString());
         mapKey(0, p1_backInput, GameConfig.p1_back.ToString());
+        mapKey(0, p1_resetInput, GameConfig.p1_reset.ToString());
 
         mapKey(0, p2_leftInput, GameConfig.p2_left.ToString());
         mapKey(0, p2_rightInput, GameConfig.p2_right.ToString());
         mapKey(0, p2_forwardInput, GameConfig.p2_forward.ToString());
         mapKey(0, p2_backInput, GameConfig.p2_back.ToString());
+        mapKey(0, p2_resetInput, GameConfig.p2_reset.ToString());
 
         p1_leftInput.onClick.AddListener(() => mapKey(1, p1_leftInput));
         p1_rightInput.onClick.AddListener(() => mapKey(2, p1_rightInput));
         p1_forwardInput.onClick.AddListener(() => mapKey(3, p1_forwardInput));
         p1_backInput.onClick.AddListener(() => mapKey(4, p1_backInput));
+        p1_resetInput.onClick.AddListener(() => mapKey(5, p1_resetInput));
 
-        p2_leftInput.onClick.AddListener(() => mapKey(5, p2_leftInput));
-        p2_rightInput.onClick.AddListener(() => mapKey(6, p2_rightInput));
-        p2_forwardInput.onClick.AddListener(() => mapKey(7, p2_forwardInput));
-        p2_backInput.onClick.AddListener(() => mapKey(8, p2_backInput));
+        p2_leftInput.onClick.AddListener(() => mapKey(6, p2_leftInput));
+        p2_rightInput.onClick.AddListener(() => mapKey(7, p2_rightInput));
+        p2_forwardInput.onClick.AddListener(() => mapKey(8, p2_forwardInput));
+        p2_backInput.onClick.AddListener(() => mapKey(9, p2_backInput));
+        p2_resetInput.onClick.AddListener(() => mapKey(10, p2_resetInput));
 
         returnBtn.onClick.AddListener(() => {
             SceneManager.LoadScene("MainMenuScene");
@@ -81,20 +87,28 @@ public class KeyBindingsUI : MonoBehaviour
                 mapKey(0, p1_backInput, pressedKey.ToString());
                 break;
             case 5:
+                GameConfig.p1_reset = pressedKey;
+                mapKey(0, p1_resetInput, pressedKey.ToString());
+                break;
+            case 6:
                 GameConfig.p2_left = pressedKey;
                 mapKey(0, p2_leftInput, pressedKey.ToString());
                 break;
-            case 6:
+            case 7:
                 GameConfig.p2_right = pressedKey;
                 mapKey(0, p2_rightInput, pressedKey.ToString());
                 break;
-            case 7:
+            case 8:
                 GameConfig.p2_forward = pressedKey;
                 mapKey(0, p2_forwardInput, pressedKey.ToString());
                 break;
-            case 8:
+            case 9:
                 GameConfig.p2_back = pressedKey;
                 mapKey(0, p2_backInput, pressedKey.ToString());
+                break;
+            case 10:
+                GameConfig.p2_reset = pressedKey;
+                mapKey(0, p2_resetInput, pressedKey.ToString());
                 break;
         }
     }
